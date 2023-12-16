e9aab229a12a0f4a6a2400fcab4118efd1a3c199
Otávio Luis Martins <otavioluism97@hotmail.com>
2023-12-15 22:32:31 -0300

    Aula 10 - Injencao de dependencia

---
+++ aula_10_injecao_dependencia.py
@@ -0,0 +1,30 @@
+class Casa(object):
+
+    def __init__(self):
+        self.__endereco = 'Rua Governador Basconcelos'
+
+    def acender_luz(self):
+        print('Acendendo a luz da casa')
+
+    def get_endereco(self) -> str:
+        return self.__endereco
+
+
+class Pessoa(object):
+
+    def __init__(self, nome: str, local: Casa) -> None:
+        self.__local = local
+        self.__nome = nome
+
+    def entrar_no_local(self) -> None:
+        self.__local.acender_luz()
+
+    def apresentar_local(self) -> None:
+        print(self.__local.get_endereco())
+
+
+casa_de_amigo = Casa()
+pessoa = Pessoa('Felipe', casa_de_amigo)
+
+pessoa.entrar_no_local()
+pessoa.apresentar_local()
